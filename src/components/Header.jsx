3eb05493f579a5c5fb1864b8207cc8181ea1966b
tenjodiego961 <79155327+tenjodiego961@users.noreply.github.com>
2021-07-09 12:13:10 -0500

    Sorting folders and files

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../assets/styles/components/Header.css";
+
+const Header = () => {
+    return(
+        <header>
+            header
+        </header>
+    )
+}
+
+export default Header;
